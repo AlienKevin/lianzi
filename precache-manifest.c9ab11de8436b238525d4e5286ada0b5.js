a62b36384f977dbc81f4d41596e66de8db9d4f5e
Kevin Li <kevinli020508@gmail.com>
2020-10-31 11:34:09 +0800

    Updates

---
+++ precache-manifest.c9ab11de8436b238525d4e5286ada0b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426c1e9b69efc18eec36ecc4a23c3221",
+    "url": "/lianzi/edukai.ttf"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/lianzi/favicon.ico"
+  },
+  {
+    "revision": "ec95a9de1cced2b2deeaf0064dfc5e71",
+    "url": "/lianzi/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/lianzi/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/lianzi/manifest.json"
+  },
+  {
+    "revision": "4e448cc77897cea6ea90",
+    "url": "/lianzi/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "4e448cc77897cea6ea90",
+    "url": "/lianzi/static/js/main.4e448cc7.chunk.js"
+  },
+  {
+    "revision": "05068ed6646d135910b5",
+    "url": "/lianzi/static/js/runtime~main.05068ed6.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/lianzi/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
